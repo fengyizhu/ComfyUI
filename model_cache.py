85de0493b768eef5e3d2c854ebdc9c06edd39891
shenenqing001 <shenenqing001@ke.com>
2024-06-19 21:26:44 +0800

    fix

--- model_cache.py
+++ model_cache.py
@@ -3,112 +3,111 @@ from collections import OrderedDict
 import os
 import logging
 import sys
+import time
 
 import psutil
-import GPUtil
+import torch
 
 from comfy.cli_args import args
 
+current_device = torch.cuda.current_device()
+gpu_total_memory = torch.cuda.get_device_properties(current_device).total_memory
+cpu_total_memeory = psutil.virtual_memory().total
 
 class ModelCache:
-    def __init__(self):
-        self._cache = OrderedDict()
-        # ignore gpu and highvram state, may cause OOM error
-        # self.cpu_device_size = psutil.virtual_memory().total * 0.8
-        self.cpu_device_size = 100
-        self.gpu_device_size = GPUtil.getGPUs()[3].memoryTotal
+    def __init__(self, cpu_device_size_ratio, gpu_device_size_ratio, cache_state=True):
+        self.cpu_cache = OrderedDict()
+        self.cpu_cache_time = OrderedDict()
+        self.cpu_device_size = cpu_total_memeory / (1024 * 1024) * cpu_device_size_ratio
+        self.gpu_device_size = gpu_total_memory * gpu_device_size_ratio
         self.gpu_cache = []
-        self.cache_size = 0
-        self._cache_state = True
+        self.cpu_cache_size = 0
+        self._cache_state = cache_state
 
-    def _get_item(self, key):
-        if self._cache.get(key) is None:
-            self._cache[key] = {}
 
-        return self._cache[key]
+    def _get_cache_model(self, key):
+        if self.cpu_cache.get(key) is None:
+            self.cpu_cache[key] = {}
+
+        return self.cpu_cache[key]
+
+
+    def _get_cache_model_time(self, key):
+        if self.cpu_cache_time.get(key) is None:
+            self.cpu_cache_time[key] = time.time()
+
+        return self.cpu_cache_time[key]
+
 
     def is_model_in_cache(self, key):
-        return key in self._cache
+        return key in self.cpu_cache
+
 
     def current_cpu_device_size(self):
         return psutil.virtual_memory().used
 
-    def current_gpu_device_size(self):
-        return GPUtil.getGPUs()[3].memoryUsed / GPUtil.getGPUs()[3].memoryTotal
-    
-    def check_model_size(self, model):
+
+    def current_gpu_device_size_ratio(self):
+        return torch.cuda.memory_allocated(current_device) / gpu_total_memory
+
+
+    def _check_model_size(self, model):
         if sys.getsizeof(model) >= self.cpu_device_size:
             logging.error("Model too large for device")
             raise Exception("Model too large for device")
         
-        while self.cache_size + sys.getsizeof(model) >= self.cpu_device_size:
+        while self.cpu_cache_size + sys.getsizeof(model) >= self.cpu_device_size:
             logging.info("Cache full, removing least recently used model")
             self.unload_last_model()
 
-    def cache_model(self, key, model):
+
+    def _cache_model(self, key, model, prop):
         if not self._cache_state:
             return
         
-        self.check_model_size(model)
-        
-        self.cache_size += sys.getsizeof(model)
-        logging.info(f"Adding model to cache")
-        item = self._get_item(key)
-        item['model'] = model
+        self._check_model_size(model)
+
+        self.cpu_cache_size += sys.getsizeof(model)
+        logging.info(f"Adding {prop} model to cache")
+        self._get_cache_model(key)[prop] = model
+        self._get_cache_model_time(key)
+
+
+    def cache_model(self, key, model):
+        self._cache_model(key, model, 'model')
+
 
     def cache_clip_vision(self, key, clip_vision):
-        if not self._cache_state:
-            return
-        
-        self.check_model_size(clip_vision)
+        self._cache_model(key, clip_vision, 'clip_vision')
 
-        self.cache_size += sys.getsizeof(clip_vision)
-        logging.info(f"Adding model to cache")
-        item = self._get_item(key)
-        item['clip_vision'] = clip_vision
 
     def cache_vae(self, key, vae):
-        if not self._cache_state:
-            return
-        
-        self.check_model_size(vae)
+        self._cache_model(key, vae, 'vae')
 
-        self.cache_size += sys.getsizeof(vae)
-        logging.info(f"Adding model to cache {key}")
-        item = self._get_item(key)
-        item['vae'] = vae
 
     def cache_sd(self, key, sd):
-        if not self._cache_state:
-            return
         assert isinstance(sd, dict)
         keys = list(sd.keys())
         values = list(sd.values())
+        self._cache_model(key, (keys, values), 'sd')
 
-        logging.info(f"Adding model to cache {key}")
-        item = self._get_item(key)
-        item['sd'] = (keys, values)
 
     def cache_clip(self, key, clip_key, clip):
-        self.check_model_size(clip)
+        self._cache_model(key, clip, clip_key)
 
-        self.cache_size += sys.getsizeof(clip)
-        logging.info(f"Adding model to cache {key}")
-        item = self._get_item(key)
-        item[clip_key] = clip
 
     def refresh_cache(self, key):
-        if key in self._cache:
-            self._cache.move_to_end(key)
+        if key in self.cpu_cache:
+            self.cpu_cache.move_to_end(key)
 
-    def get_item(self, key, prop):
-        item = self._cache.get(key)
+    def get_model(self, key, prop):
+        item = self.cpu_cache.get(key)
         if item is None:
             logging.info(f"Model not in cache {key}")
             return None
 
-        self.refresh_cache(key)
         logging.info(f"Retrieved model from cache {key}")
+        self.cpu_cache_time[key] = time.time()
 
         if prop == "sd":
             if item.get('sd') is None:
@@ -118,25 +117,28 @@ class ModelCache:
         return item.get(prop)
     
     def __size__(self):
-        return sys.getsizeof(self._cache)
+        return sys.getsizeof(self.cpu_cache)
 
     def __len__(self):
-        return len(self._cache)
+        return len(self.cpu_cache)
 
     @property
     def cache(self):
-        return self._cache
+        return self.cpu_cache
 
     @staticmethod
     def unpatch_offload_model(model):
         model.model_patches_to(model.offload_device)
 
+
     def unload_last_model(self):
-        if self.cache_size == 0:
+        if self.cpu_cache_size == 0:
             return
 
-        cache_k, item = self._cache.popitem(last=False)
-        self.cache_size -= sys.getsizeof(item)
+        unload_key = min(self.cpu_cache_time, key=self.cpu_cache_time.get)
+        cache_k, item = self.cpu_cache.pop(unload_key)
+        self.cpu_cache_time.pop(unload_key)
+        self.cpu_cache_size = self.__size__()
         item.pop("sd", None)
 
         for k in list(item.keys()):
@@ -154,4 +156,4 @@ class ModelCache:
         logging.info(f"Drop model cache: {model_name} ({dir_name})")
 
 
-model_cache = ModelCache()
+model_cache = ModelCache(0.8,0.8)
